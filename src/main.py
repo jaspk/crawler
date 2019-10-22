3069212acda0f765479dec29d9ffe7785d2ae13d
grestonian <gsingh0719@gmail.com>
2019-10-23 03:37:25 +0530

    testing

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
 NUMBER_OF_THREADS = 8
 
+
+print('hello')
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
 
